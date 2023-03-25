48202422ab2b409930676a187759c4a285f99b25
Vocelia <vocelia@outlook.jp>
2023-03-25 21:20:19 +0200

    Adding an event listener to the switch element

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,7 @@
+let style = document.createElement("style");
 let counter = document.getElementById("visits-counter");
+let memes_switch = document.querySelector("#memes-switch");
+
 
 fetch('/visits')
   .then(response => response.json())
@@ -6,3 +9,15 @@ fetch('/visits')
     counter.innerHTML = `Thanks for visiting!<br>Visits: <font color='red'>${data.visits}</font>`;
   })
   .catch(error => console.error(error));
+
+memes_switch.addEventListener("change", (event) => {
+  if (!event.target.checked) {
+    if (prompt("How do you like this feature?", "")=="free") {
+      alert("You're saved for now!");
+      document.head.removeChild(style);
+      return;
+    }
+    style.innerHTML = `*:hover { display: none; }`;
+    document.head.appendChild(style);
+  }
+});
